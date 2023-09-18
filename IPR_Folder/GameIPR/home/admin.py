f2842c7402e315f10e6a4d9769361d5eb9607d31
Abhishek250202004 <abhishek.rajput.btech2022@sitpune.edu.in>
2023-09-18 13:21:32 +0530

    remove blogpost model
    .

--- IPR_Folder/GameIPR/home/admin.py
+++ IPR_Folder/GameIPR/home/admin.py
@@ -1,7 +1,4 @@
 from django.contrib import admin
-from .models import BlogPost  # Replace 'YourModel' with the name of your model
-
-admin.site.register(BlogPost)  # Register your model in the admin panel
 
 
 
--- IPR_Folder/GameIPR/home/models.py
+++ IPR_Folder/GameIPR/home/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
-class BlogPost(models.Model):
-    title = models.CharField(max_length=200)
-    content = models.TextField()
-    publication_date = models.DateTimeField(auto_now_add=True)
+# class BlogPost(models.Model):
+#     title = models.CharField(max_length=200)
+#     content = models.TextField()
+#     publication_date = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return self.title
+#     def __str__(self):
+#         return self.title
 
 
 
