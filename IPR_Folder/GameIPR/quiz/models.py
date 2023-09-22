2cbc1dd259df1ccd398e2e603cfe58d3a8a0c642
capricode-ui <110791135+capricode-ui@users.noreply.github.com>
2023-09-22 20:42:37 +0530

    changed cpr.html
    cpr.html

--- IPR_Folder/GameIPR/quiz/models.py
+++ IPR_Folder/GameIPR/quiz/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class QuizQuestion(models.Model):
     question_text = models.CharField(max_length=700)
     question_image=models.ImageField(null=True ,blank=True )
+    image_url = models.URLField(null=True,blank=True)
     option1 = models.CharField(max_length=100)
     option2 = models.CharField(max_length=100)
     option3 = models.CharField(max_length=100)
