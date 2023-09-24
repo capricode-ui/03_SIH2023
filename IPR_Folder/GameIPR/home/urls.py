e585f701c1dfd749fe3c9ede513ed98e5b8a68c1
Abhishek-2502 <abhishek25022004@gmail.com>
2023-09-24 22:12:40 +0530

    Done with Login sign up. Didn't included css

--- IPR_Folder/GameIPR/home/urls.py
+++ IPR_Folder/GameIPR/home/urls.py
@@ -1,13 +1,24 @@
 from django.contrib import admin
 from django.urls import path 
 
-from home import views
+# from home import views
 
+from django.contrib.auth import views as auth_views
+from .views import index,login, signup, logout_user
 
 urlpatterns = [
-   path("",views.index,name='home'),
-   path("login",views.login,name='login'),
-   path("signup",views.signup,name='signup')
+   # path("",views.index,name='home'),
+   # path("login",views.login,name='login'),
+   # path("signup",views.signup,name='signup'),
+
+   path("",index,name='index'),
+   path("login",login,name='login'),
+   path("signup",signup,name='signup'),
+   
+
+   # path("register", register, name="register"),
+   # path("login_user", login_user, name="login_user"),
+   path("logout_user", logout_user, name="logout_user"),
    
    
  
