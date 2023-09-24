150fac215797716a600659ddd5312a273d2d5c96
Janmejay-Pandya <jay110504@gmail.com>
2023-09-25 02:42:29 +0530

    signup_page
    -

--- IPR_Folder/GameIPR/home/views.py
+++ IPR_Folder/GameIPR/home/views.py
@@ -15,12 +15,12 @@ def index(request):
 
 def signup(request):
     if request.method == 'POST':
-        first_name = request.POST['first_name']
-        last_name = request.POST['last_name']
-        username = request.POST['username']
-        email =request.POST['email']
-        password = request.POST['password']
-        confirm_password = request.POST['confirm_password']
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        username = request.POST.get('username')
+        email =request.POST.get('email')
+        password = request.POST.get('password')
+        confirm_password = request.POST.get('confirm_password')
         if password==confirm_password:
             if User.objects.filter(username=username).exists():
                 messages.info(request, 'Email is exist ')
@@ -41,8 +41,8 @@ def signup(request):
 
 def login(request):
     if request.method == 'POST':
-        username =request.POST['username']
-        password = request.POST['password']
+        username =request.POST.get('username')
+        password = request.POST.get('password')
         user = auth.authenticate(username=username, password=password)
         if user is not None:
             auth.login(request, user)
