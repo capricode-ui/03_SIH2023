1c4d34df38c97e62a16098c0c25701c2a91efdfb
capricode-ui <110791135+capricode-ui@users.noreply.github.com>
2023-09-24 23:19:08 +0530

    -
    -

--- IPR_Folder/GameIPR/quiz/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import QuizQuestion  # Replace 'YourModel' with the name of your model
-
-admin.site.register(QuizQuestion)  # Register your model in the admin panel
-
-# Register your models here.
